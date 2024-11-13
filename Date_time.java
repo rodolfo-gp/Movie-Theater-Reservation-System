7d88b1f6f44c9679ef3b8db5a825242c827e3d10
s0uptim3 <odinboi345@gmail.com>
2024-11-13 11:59:18 -0700

    Added Date_time.java
    Use for when you need to reference a date in your class

---
+++ Date_time.java
@@ -0,0 +1,38 @@
+public class Date_time {
+    public int month;
+    public int day;
+    public Integer hour; // Set to Integer to allow null values
+    public Integer minute;
+
+    public Date_time(int month, int day, int hour, int minute) {
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    //second ctor for when you don't need minute and hour (movie release dates)
+    public Date_time(int month, int day) {
+        this.month = month;
+        this.day = day;
+        this.hour = null;    
+        this.minute = null;
+    }
+
+    
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public Integer getMinute() {
+        return minute;
+    }
+}
