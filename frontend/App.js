ae4afbd3085b07abd83030c09b5638d54ecdc4fc
s0uptim3 <odinboi345@gmail.com>
2024-11-25 10:23:28 -0700

    removed previous web code

--- frontend/App.js
+++
@@ -1,6 +0,0 @@
-import { fetchMovies } from "./api.js";
-import { renderMovies } from "./render.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetchMovies().then(renderMovies).catch(console.error);
-});
