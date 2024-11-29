b535fe19dfa421227fc5ea5d12ead65e7e9f9f6a
Inko-z <rodolfoandresgp@hotmail.com>
2024-11-29 14:19:49 -0700

    Updated date_time class to have some extra functionality

--- frontend/Date_time.java
+++ frontend/Date_time.java
@@ -1,4 +1,5 @@
 package frontend;
+
 public class Date_time {
     public int year;
     public int month;
@@ -6,6 +7,7 @@ public class Date_time {
     public Integer hour; // Set to Integer to allow null values
     public Integer minute;
 
+    // Constructor when hour and minute are provided
     public Date_time(int month, int day, int hour, int minute) {
         this.year = 0;
         this.month = month;
@@ -14,28 +16,29 @@ public class Date_time {
         this.minute = minute;
     }
 
-    //second ctor for when you don't need minute and hour (movie release dates)
+    // Constructor when only month and day are provided (for release date)
     public Date_time(int month, int day) {
         this.year = 0;
         this.month = month;
         this.day = day;
-        this.hour = null;    
+        this.hour = null;
         this.minute = null;
     }
 
-    //for when you need a release date (just year)
+    // Constructor when only year is provided (for year-only date)
     public Date_time(int year) {
         this.year = year;
         this.month = 0;
         this.day = 0;
-        this.hour = null;    
+        this.hour = null;
         this.minute = null;
-    } 
+    }
 
+    // Getters
     public int getYear() {
         return year;
     }
-    
+
     public int getMonth() {
         return month;
     }
@@ -52,15 +55,44 @@ public class Date_time {
         return minute;
     }
 
+    // Display time as a String
     public String displayTime() {
         String time = null;
         if (year == 0) {
-            time = month + "/" + day + " " + hour + ":" + minute;
-            return time;
+            if (hour != null && minute != null) {
+                time = month + "/" + day + " " + hour + ":" + (minute < 10 ? "0" + minute : minute);
+            } else {
+                time = month + "/" + day;
+            }
+        } else {
+            time = year + (month == 0 && day == 0 ? "" : "-" + String.format("%02d", month) + "-" + String.format("%02d", day));
         }
-        else {
-            time = String.valueOf(year);
-            return time;
+        return time;
+    }
+
+    // Static method to parse a date string in the format "YYYY-MM-DD" or "YYYY-MM-DDTHH:MM"
+    public static Date_time parseDate(String dateString) {
+        if (dateString.contains("T")) {
+            // Date with time, e.g. "2024-11-29T15:30"
+            String[] dateTimeParts = dateString.split("T");
+            String[] dateParts = dateTimeParts[0].split("-");
+            String[] timeParts = dateTimeParts[1].split(":");
+
+            int year = Integer.parseInt(dateParts[0]);
+            int month = Integer.parseInt(dateParts[1]);
+            int day = Integer.parseInt(dateParts[2]);
+            int hour = Integer.parseInt(timeParts[0]);
+            int minute = Integer.parseInt(timeParts[1]);
+
+            return new Date_time(month, day, hour, minute);
+        } else {
+            // Date without time, e.g. "2024-11-29"
+            String[] dateParts = dateString.split("-");
+            int year = Integer.parseInt(dateParts[0]);
+            int month = Integer.parseInt(dateParts[1]);
+            int day = Integer.parseInt(dateParts[2]);
+
+            return new Date_time(month, day);
         }
     }
 }
