dcb89cf006730d25ac56ef8b92188978919c165f
s0uptim3 <odinboi345@gmail.com>
2024-11-28 19:26:10 -0700

    Implemented purchase ticket

--- frontend/PurchaseTicket.java
+++ frontend/PurchaseTicket.java
@@ -14,11 +14,33 @@ public class PurchaseTicket {
         purchasePage.setLayout(new BorderLayout());
 
         //if user not logged in 
-        JTextField email = new JTextField();
-        JTextField card_digits = new JTextField();
-        JTextField ccv_code = new JTextField();
-        JButton submit = new JButton();
+        JTextField email = new JTextField(BorderLayout.PAGE_START);
+        JTextField card_digits = new JTextField(BorderLayout.CENTER);
+        JTextField ccv_code = new JTextField(BorderLayout.PAGE_END);
+        JButton submit = new JButton(BorderLayout.LINE_END);
 
+        String emailv = email.getText();
+        String card_digitsv = card_digits.getText();
+        String ccv_codev = ccv_code.getText();
+
+
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (emailv != null && card_digitsv != null && ccv_codev != null ) {
+                    //blah blah blah
+                }
+                
+                    
+            }
+        });
+
+        purchasePage.add(email);
+        purchasePage.add(card_digits);
+        purchasePage.add(ccv_code);
+        purchasePage.add(submit);    
+        
+        purchasePage.setVisible(true);
         
     }
 }
